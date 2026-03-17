939f9268fad6ca99075b401db5556ebc0db2b719
Your Name <your@email.com>
2026-03-17 23:29:55 +0530

    Updated sum program

--- sum.py
+++ sum.py
@@ -1,5 +1,8 @@
+# sum of two numbers
 a = int(input("enter first no:"))
 b = int(input("enter second no:"))
 c = int(input("enter third no:"))
 total = a+b+c
 print("sum =",total)
+
+# sum of three numbers
